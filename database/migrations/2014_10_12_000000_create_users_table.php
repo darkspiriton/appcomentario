841ef1c5ec4ffa18fec1826dda842622ee880f54
Cesar Moreno <cesar200526@gmail.com>
2016-04-11 18:55:05 -0500

    Se agregaron las migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_online', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email')->unique();            
+            $table->boolean('status');
+            $table->timestamps();
+        });
+
+        Schema::create('users_register', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');  
             $table->timestamps();
         });
     }
---
+++ database/migrations/2016_04_11_233812_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->string('email')->index();
+            $table->string('comment')->index();
+            $table->bolean('status');
+            $table->integer('stars');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
