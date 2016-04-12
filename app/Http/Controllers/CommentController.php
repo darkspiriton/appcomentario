bfce50bcc0ff408c1d0552a208623506d035b74f
Cesar Moreno <cesar200526@gmail.com>
2016-04-12 14:18:46 -0500

    Prueba de acceso dominio 2

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,7 +10,7 @@ use App\Comment;
 class CommentController extends Controller
 {
     public function index(){
-        header("Access-Control-Allow-Origin");
+        header("Access-Control-Allow-Origin: http://dev.nosvenden.com");
         $comments = DB::table('comments')->where('status','1')->orderBy('created_at','desc')->get();
         return response()->json(['comments' => $comments],200);
         
