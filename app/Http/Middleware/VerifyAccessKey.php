e838249a5e47b6d3567f5a564bb0ed3859126d0c
Cesar Moreno <cesar200526@gmail.com>
2016-04-13 15:11:32 -0500

    se agrego middleware web para validad codigo api

---
+++ app/Http/Middleware/VerifyAccessKey.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Config;
+
+class VerifyAccessKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Obtenemos el api-key que el usuario envia
+        $key = $request->headers->get('api_key');
+        // Si coincide con el valor almacenado en la aplicacion
+        // la aplicacion se sigue ejecutando
+        if (isset($key) == Config::get('app.api_key')) {
+            return $next($request);
+        } else {
+            // Si falla devolvemos el mensaje de error
+            return response()->json(['error' => 'unauthorized' ], 401);
+        }
+    }
+}
