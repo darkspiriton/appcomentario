559e406d94e3dffb4d153e3daea7673f22056328
Cesar Moreno <cesar200526@gmail.com>
2016-04-14 15:46:50 -0500

    Se agrego header

--- app/Http/Middleware/VerifyAccessKey.php
+++ app/Http/Middleware/VerifyAccessKey.php
@@ -17,7 +17,7 @@ class VerifyAccessKey
     public function handle($request, Closure $next)
     {
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+        header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,HEAD");
         header("Access-Control-Allow-Headers: Content-Type");
         header("Content-Type: application/json");
         
