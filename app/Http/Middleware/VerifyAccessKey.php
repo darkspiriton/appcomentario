c7e5ef12b1bed63cb2fe766450204cba7453b7c7
Cesar Moreno <cesar200526@gmail.com>
2016-04-14 22:27:34 -0500

    Prueba de acceso a API remoto 2

--- app/Http/Middleware/VerifyAccessKey.php
+++ app/Http/Middleware/VerifyAccessKey.php
@@ -16,7 +16,7 @@ class VerifyAccessKey
      */
     public function handle($request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: http://localhost:8000");
         header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS, HEAD");
         header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin");
         header('Access-Control-Allow-Credentials: true');
