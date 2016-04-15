147a02185ea08b6e8f76eb794fe6e90c503110b5
Cesar Moreno <cesar200526@gmail.com>
2016-04-14 23:05:31 -0500

    Prueba de acceso a API remoto 8

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@
 |
 */
 // allow origin
-//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
 // add any additional headers you need to support here
 //header('Access-Control-Allow-Headers: Origin, Content-Type');
 
